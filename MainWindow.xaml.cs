5051aec488404e9a54255302529b21cbbfffc0f8
Loki <lokiabutton@gmail.com>
2025-07-19 17:48:54 +0100

    Adds playbar

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,56 +9,31 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
 
 namespace Audio_visual_app {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
-        public MainWindow(){
-            InitializeComponent();
-        }
-
         // Audio variables
         static WaveOutEvent? outputDevice;
         static AudioFileReader? audioFile;
-        string filename;
+
+        string? filename;
+
         private bool playing = false;
+        DispatcherTimer? dispatcherTimer;
 
-        /// <summary>
-        /// When playback stops
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="args"></param>
-       void OnPlaybackStopped(object? sender, StoppedEventArgs args){
-            if (outputDevice != null){
-                outputDevice.Dispose();
-                outputDevice = null;
-            }
+        public MainWindow(){
+            InitializeComponent();
 
-            if (audioFile != null){
-                audioFile.Dispose();
-                audioFile = null;
-            }
-
-            playing = false;
+            filename = selectfile();
+            if (filename != null) { initialise();  }
         }
 
-        /// <summary>
-        /// Get current time of song
-        /// </summary>
-        /// <param name="af"></param>
-        /// <returns></returns>
-        public static double get_time(AudioFileReader af) {
-            return af.CurrentTime / af.TotalTime;
-        }
-
-        /// <summary>
-        /// When the button is clicked
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void Button_Click(object sender, RoutedEventArgs e){
+        string? selectfile() {
             Console.WriteLine("Selecting file...");
 
             // Configure open file dialog box
@@ -66,45 +41,62 @@ namespace Audio_visual_app {
             dialog.Filter = "Audio files|*.wav;*.mp3"; // Filter files by extension
 
             // Show open file dialog box
-            bool? result = dialog.ShowDialog();
+            dialog.ShowDialog();
 
-            // Process open file dialog box results
-            if (result == true) {
-                // Open document
-                filename = dialog.FileName;
-
-                // Play file via NAudio
-                if (outputDevice == null) {
-                    outputDevice = new WaveOutEvent();
-                    outputDevice.PlaybackStopped += OnPlaybackStopped;
-                }
-
-                if (audioFile == null) {
-                    audioFile = new AudioFileReader(filename);
-                    outputDevice.Init(audioFile);
-                }
-
-                // Store PCM Data
-                //WaveStream ws = new Mp3FileReader(filename);
-                //WaveStream pcm = WaveFormatConversionStream.CreatePcmStream(ws);
-                //int interval = pcm.WaveFormat.Channels * pcm.WaveFormat.SampleRate * pcm.WaveFormat.BitsPerSample / 8;
-
-                // Start a timer
-                System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
-                dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
-                dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 1);
-                dispatcherTimer.Start();
-            }
+            return dialog.FileName;
         }
-        
+
+        void initialise() {
+            // Store PCM Data
+            //WaveStream ws = new Mp3FileReader(filename);
+            //WaveStream pcm = WaveFormatConversionStream.CreatePcmStream(ws);
+            //int interval = pcm.WaveFormat.Channels * pcm.WaveFormat.SampleRate * pcm.WaveFormat.BitsPerSample / 8;
+
+            // Start a timer
+            dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 1);
+            dispatcherTimer.Start();
+
+            // initialise audio variables
+            outputDevice = new WaveOutEvent();
+            outputDevice.PlaybackStopped += OnPlaybackStopped;
+            
+            audioFile = new AudioFileReader(filename);
+            outputDevice.Init(audioFile);
+
+            // set slider
+            slider1.Maximum = audioFile.TotalTime.TotalMilliseconds;
+        }
+
+        /// <summary>
+        /// When playback stops
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+       void OnPlaybackStopped(object? sender, StoppedEventArgs args){
+            //outputDevice.Dispose();
+            //outputDevice = null;
+            //audioFile.Dispose();
+            //audioFile = null;
+
+            playing = false;
+            audioFile.Position = 0;
+        }
+
         private void dispatcherTimer_Tick(object sender, EventArgs e) {
             if (playing) {
-                label1.Content = Math.Round(get_time(audioFile)*100)+"%";
+                label1.Content = audioFile.CurrentTime;
+                slider1.Value = audioFile.CurrentTime.TotalMilliseconds;
             }
         }
 
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
-
+            //if (playing && audioFile.Position != (long)slider1.Value) {
+                //dispatcherTimer.Stop();
+                //audioFile.Position = (long)slider1.Value;
+                //playing = false;
+            //}
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e) {
