c16ee628381300af76af669bf647b9f1f21a91f3
Loki <lokiabutton@gmail.com>
2025-07-19 11:13:40 +0100

    Re-state for gitignore

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Audio_visual_app
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
