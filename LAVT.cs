96cd98ebcfb6e868ed402acfbd080130ca23e3af
Loki <lokiabutton@gmail.com>
2025-07-24 15:20:23 +0100

    Loading bar added

--- LAVT.cs
+++ LAVT.cs
@@ -1,19 +1,31 @@
 using FftSharp;
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.ApplicationServices;
+using Microsoft.VisualBasic.Devices;
 using NAudio.Wave;
+using NAudio.Wave.Compression;
 using NAudio.Wave.SampleProviders;
 using System;
 using System.IO;
 using System.Security.Policy;
 using System.Windows;
+using System.Windows.Media;
 using System.Windows.Threading;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Audio_visual_app {
     public static class LAVT { // Loki's audio visual toolkit
         // Audio variables
         private static WaveOutEvent outputDevice;
         private static Mp3FileReader mp3reader;
+        
+        // Playing?
         private static bool playing = false;
-        private static List<byte[]> data; 
+
+        // PCM data
+        private static WaveStream PCMstream;
+        private static double[][] PCMSamples;
+        private static bool gotPCMData;
 
         /// <summary>
         /// Initialise LAVT (Loki's audio visual toolkit)
@@ -28,41 +40,106 @@ namespace Audio_visual_app {
             mp3reader = new Mp3FileReader(filename);
             outputDevice.Init(mp3reader);
 
-            // Initialise data list
-            data = new List<byte[]>();
+            // Get array of pcm samples
+            PCMstream = NAudio.Wave.WaveFormatConversionStream.CreatePcmStream(mp3reader);
+            gotPCMData = false;
+            GetPCMArray();
+            
+        }
+
+        public static bool hasPCMData() {
+            return gotPCMData && GetCurrentTime().TotalSeconds < GetTotalTime().TotalSeconds;
+        }
+
+        public static double[][] GetPCMSamples() {
+            return PCMSamples;
+        }
+
+        private static byte[] GetByte() {         
+            byte[] buffer = new byte[8];
+            PCMstream.Read(buffer);
+            
+            return buffer;
+        }
+
+        public static double GetBytePCM() {
+            return BitConverter.ToDouble(GetByte(), 0);
+        }
+
+        public static double[] GetPCMSample(int size) {
+            double[] sample = new double[size];
+
+            for (int i = 0; i < size; i++) {
+                sample[i] = GetBytePCM();
+            }
+
+            return sample;
         }
 
         /// <summary>
         /// Get a sample of pcm data
         /// </summary>
-        public static double[] GetPCMSample() {
-            // creates a pcm stream
-            NAudio.Wave.WaveStream pcmstream = NAudio.Wave.WaveFormatConversionStream.CreatePcmStream(mp3reader);
+        private static void GetPCMArray() {
+            // convert the byte array to a single double
             
-            // amount of bytes in a second.
-            int second = pcmstream.WaveFormat.Channels * pcmstream.WaveFormat.SampleRate * pcmstream.WaveFormat.BitsPerSample / 8; 
+
+            /*
+            // amount of bytes that make up a second.
+            int bytesecond = (PCMstream.WaveFormat.Channels * PCMstream.WaveFormat.SampleRate * PCMstream.WaveFormat.BitsPerSample);
             
-            // buffer (millisecond in size)
-            byte[] buffer = new byte[second / 60];
+            // amount of doubles that make up a second
+            int doublesecond = bytesecond / 8;
 
-            // read ahead by size of buffer
-            pcmstream.Read(buffer);
+            // amount of seconds available
+            int seconds = (int)Math.Floor(GetTotalTime().TotalSeconds);
 
-            // define pcm array
-            double[] pcm = new double[pcmstream.WaveFormat.BitsPerSample];
+            // Full pcm array
+            double[][] samples = new double[seconds][];
 
-            // Convert bytes to floats
-            for (int i = 0; i < pcm.Length; i++) {
-                pcm[i] = BitConverter.ToDouble(buffer, 0);
-            }
+            */
 
-            // Reset reader position
-            if (GetCurrentTime() != GetTotalTime()) {
-                mp3reader.Position -= buffer.Length;
-            }
+            // DEBUG; raw bytes
+            //byte[] audiobytes = new byte[seconds * bytesecond];
+            //int f = 0;
 
-            // Return pcm data
-            return pcm;
+            // index through each second
+            /*for (int i = 0; i < samples.Length; i++) {
+                //if (GetCurrentTime().TotalSeconds != GetTotalTime().TotalSeconds) {
+                    // converted bytes to pcm array (1 second worth of data)
+                    double[] convertedtoPCM = new double[doublesecond];
+
+                    // list through a seconds worth of byte data
+                    for (int j = 0; j < convertedtoPCM.Length; j++) {
+                
+                        
+
+                        // Store the value
+                        convertedtoPCM[j] = val;
+
+                        // DEBUG
+                        /*for (int k = 0; k < buffer.Length; k++) {
+                            audiobytes[f] = buffer[k];
+                            f++;
+                        }
+                    }
+
+                    // save converted byte array to samples
+                    samples[i] = convertedtoPCM;
+                //}*/
+
+            // DEBUG; writes bytes back to wav file
+            //WaveFormat waveFormat = new WaveFormat(pcmstream.WaveFormat.SampleRate, pcmstream.WaveFormat.BitsPerSample, pcmstream.WaveFormat.Channels);
+            //using (WaveFileWriter writer = new WaveFileWriter(
+            //    "C:\\Users\\mydre\\OneDrive\\Documents\\Audio-visual-app\\Test\\test.wav", waveFormat)) {
+            //    writer.WriteData(audiobytes, 0, audiobytes.Length);
+            //}
+
+            // PCM variables
+            //PCMSamples = samples;
+            //gotPCMData = true;
+
+            // Reset audio file reader position
+            //mp3reader.Position = 0;
         }
 
         /// <summary>
@@ -164,6 +241,7 @@ namespace Audio_visual_app {
             // Calculate the FFT as an array of complex numbers
             System.Numerics.Complex[] spectrum = FFT.Forward(signal);
 
+            // return spectrum
             return spectrum;
         }
 
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using Microsoft.VisualBasic.Devices;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
@@ -8,8 +9,17 @@ namespace Audio_visual_app {
     using static LAVT;
 
     public partial class MainWindow : Window {
+        // file name
         string filename;
+
+        // timer
         static DispatcherTimer dispatcherTimer;
+
+        // audio is loading
+        bool audioLoading;
+
+        // events
+        public List<bool> onset = new List<bool>();
 
         /// <summary>
         /// Initialise window
@@ -36,6 +46,18 @@ namespace Audio_visual_app {
                 slider1.Maximum = GetTotalTime().TotalMilliseconds;
                 slider1.Minimum = 0;
                 slider1.IsMoveToPointEnabled = true;
+
+                // disable UI
+                slider1.IsEnabled = false;
+                B1.IsEnabled = false;
+                B2.IsEnabled = false;
+                B3.IsEnabled = false;
+                check1.IsEnabled = false;
+                check2.IsEnabled = false;
+                check3.IsEnabled = false;
+
+                // audio
+                audioLoading = true;
             } else {
                 MessageBox.Show("No file selected.", "No file", MessageBoxButton.OK, MessageBoxImage.Warning);
                 dump();
@@ -60,21 +82,9 @@ namespace Audio_visual_app {
             return dialog.FileName;
         }
 
-        /*private void DrawData() {
-            // Clear canvas
+        private void DrawData(double[] data) {
+            /*// Clear canvas
             canvas1.Children.Clear();
-
-            float[] data = PCM[0];
-
-            // Get PCM data
-            //System.Numerics.Complex[] FFT = PerformFFT(signal);
-            //double[] data = GetOnsets(GetPowers(FFT), (int)sensitivity.Value);
-
-            /*if (data[0] > 0) {
-                radio1.IsChecked = true;
-            } else {
-                radio1.IsChecked = false;
-            }
 
             // Setup polygon object
             Polygon myPolygon = new Polygon();
@@ -89,7 +99,7 @@ namespace Audio_visual_app {
 
             // Draw points
             for (int i = 0; i < data.Length; i++) {
-                System.Windows.Point p = new System.Windows.Point(i, data[i]);
+                System.Windows.Point p = new System.Windows.Point(i, data[i] / 10000);
                 points.Add(p);
             }
 
@@ -101,8 +111,17 @@ namespace Audio_visual_app {
 
             // Add points
             myPolygon.Points = points;
-            canvas1.Children.Add(myPolygon);
-        }*/
+            canvas1.Children.Add(myPolygon);*/
+        }
+
+        private void enable() {
+            audioLoading = false;
+            slider1.IsEnabled = true;
+            B1.IsEnabled = true;
+            B2.IsEnabled = true;
+            B3.IsEnabled = true;
+            slider1.Value = 0;
+        }
 
         /// <summary>
         /// Update GUI on timer tick
@@ -110,13 +129,76 @@ namespace Audio_visual_app {
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void dispatcherTimer_Tick(object sender, EventArgs e) {
-            // update slider GUI
-            label1.Content = GetCurrentTime();
-            slider1.Value = GetCurrentTime().TotalMilliseconds;
+            // current time
+            TimeSpan current = GetCurrentTime();
 
-            double[] sample = GetPCMSample();
-            double[] powers = GetPowers(PerformFFT(sample));
-            radio1.IsChecked = powers.Max() > 0;
+            // Loading
+            check1.IsChecked = !audioLoading;
+            slider1.Value = current.TotalMilliseconds;
+
+            // If audio is loading
+            if (audioLoading) {
+                // Percentage label
+                label1.Content = ((current.TotalMilliseconds / GetTotalTime().TotalMilliseconds) * 100).ToString("0") + "%";
+                
+                // loading tag
+                check1.Content = "Loading...";
+                
+                // populate samples
+                double[] sample = GetPCMSample(1024);
+
+                for (int i = 0; i < sample.Length; i++) {
+                    onset.Add(sample[i] > 0);
+                }
+
+                // end of load
+                if (GetCurrentTime() == GetTotalTime()) { enable(); }
+            } else {
+                // playback label
+                label1.Content = GetCurrentTime();
+
+                // loaded
+                check1.Content = "Loaded.";
+
+                // current index
+                int index = (int)current.TotalSeconds;
+                check2.IsChecked = onset[index];
+            }
+
+            // Get a sample
+            //
+
+            // Perform an fft on it
+            //System.Numerics.Complex[] FFT = PerformFFT(sample);
+
+            // Check if it's an onset
+            //int sensitivity = 10;
+            //double[] powers = GetPowers(FFT);
+            //check1.IsChecked = powers.Max() > sensitivity;
+
+            /////////////////////////////////////////////////////////////////
+            //check1.Content = "power max: " + powers.Max().ToString("0.0");
+
+            // Get sample and perform FFT
+            /*if (hasPCMData()) {
+                double[] sample = GetPCMSamples()[(int)Math.Floor(GetCurrentTime().TotalSeconds)];
+            }*/
+
+            /*System.Numerics.Complex[] FFT = PerformFFT(sample);
+
+            // Power
+            double[] powers = GetPowers(FFT);
+            //check1.IsChecked = powers.Max() > 0;
+            //check1.Content = "power max: " + powers.Max().ToString("0.0");
+
+            // Frequency
+            double[] frequencies = GetFrequencies(FFT);
+            double frequency = 0;
+            frequency = frequencies[Array.IndexOf(powers, powers.Max())];
+
+            check3.IsChecked = true;
+            check3.Content = "Frequency: " + frequency.ToString("0.0");*/
+            /////////////////////////////////////////////////////////////////
         }
 
         /// <summary>
@@ -128,7 +210,7 @@ namespace Audio_visual_app {
             Slider slider = sender as Slider;
 
             // if the the slider isn't equal to current position of the audio
-            if (slider1.Value != GetCurrentTime().TotalMilliseconds) {
+            if (slider1.Value != GetCurrentTime().TotalMilliseconds && !audioLoading) {
                 Seek((int)slider1.Value); // seek the audio to the sliders position
             }
         }
@@ -139,7 +221,9 @@ namespace Audio_visual_app {
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void Button_Click_1(object sender, RoutedEventArgs e) {
-            StartPlayback();
+            if (!audioLoading) { 
+                StartPlayback(); 
+            }
         }
 
         /// <summary>
@@ -148,7 +232,9 @@ namespace Audio_visual_app {
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void Button_Click_2(object sender, RoutedEventArgs e) {
-            PausePlayback();
+            if (!audioLoading) {
+                PausePlayback();
+            }
         }
 
         /// <summary>
@@ -157,10 +243,18 @@ namespace Audio_visual_app {
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void Button_Click_3(object sender, RoutedEventArgs e) {
-            StopPlayback();
+            if (!audioLoading) {
+                StopPlayback();
+            }
         }
 
         private void RadioButton_Checked(object sender, RoutedEventArgs e) {
+
+        }
+
+        
+
+        private void check1_Checked(object sender, RoutedEventArgs e) {
 
         }
     }
