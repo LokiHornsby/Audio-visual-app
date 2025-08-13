e805cdeb26f087f86aa953b18131dfd4cd6b40a7
Loki <lokiabutton@gmail.com>
2025-08-13 17:11:38 +0100

    #44

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -10,8 +10,8 @@ using System.Windows.Threading;
 
 namespace Audio_visual_app {
     public partial class MainWindow : Window {
-        // file
-        string filename = "";
+        // load
+        bool loaded;
 
         // active
         bool active = false;
@@ -23,125 +23,59 @@ namespace Audio_visual_app {
         System.Timers.Timer timerGUI;
 
         // quality
-        int quality = 10;
+        int quality = 100;
 
         // visuals
         int i;
         int linepos;
 
-        // loaded
-        bool loaded = false;
+        public void setInteract(bool x) {
+            F1.IsEnabled = x;
+            qslider.IsEnabled = x;
+            Pslider.IsEnabled = x;
+            B1.IsEnabled = x;
 
-        void setLoading() {
-            loaded = false;
+            /*canvas1.Children.Clear();
 
-            // Loading
-            B1.IsEnabled = false;
+            band1.IsEnabled = x;
+            band2.IsEnabled = x;
+            band3.IsEnabled = x;
+            band4.IsEnabled = x;
+            band5.IsEnabled = x;
 
-            check1.IsEnabled = false;
-            check1.IsChecked = false;
+            if (x) {
+                canvas1.VerticalAlignment = VerticalAlignment.Bottom;
+                canvas1.HorizontalAlignment = HorizontalAlignment.Left;
 
-            check1.Content = "Loading...";
-            check2.IsEnabled = false;
-            check2.IsChecked = false;
-
-            check2.Content = "Loading...";
-            check3.IsEnabled = false;
-            check3.IsChecked = false;
-
-            check3.Content = "Loading...";
-            slider1.IsEnabled = false;
-
-            band1.IsEnabled = false;
-            band2.IsEnabled = false;
-            band3.IsEnabled = false;
-            band4.IsEnabled = false;
-            band5.IsEnabled = false;
-
-            qslider.Maximum = 200; // 200 milliseconds : 5 data samples
-            qslider.Minimum = 20;  // 100 : 10
-                                   // 50 : 20
-                                   // 20 : 50
-
-            qslider.IsEnabled = true;
-            //qslider.IsEnabled = false;
-            qslider.IsMoveToPointEnabled = true;
-
-            // draw waveform
-            canvas1.Children.Clear();
-            canvas1.VerticalAlignment = VerticalAlignment.Bottom;
-            canvas1.HorizontalAlignment = HorizontalAlignment.Left;
-
-            for (int i = 0; i < LAVT.samplesize; i++) {
-                Line line = new Line();
-                line.StrokeThickness = 1;
-                line.Stroke = System.Windows.Media.Brushes.Black;
-                line.X1 = i;
-                line.X2 = i;
-                line.Y1 = 0;
-                line.Y2 = 15;
-                canvas1.Children.Add(line);
-            }
-        }
-
-        public void setQuality(int q) {
-            quality = q;
-            LAVT.setData(q);
-
-            wait = 1000 / q;
-            setTimer(wait);
-        }
-
-        public void setTimer(int ms) {
-            if (timerGUI != null) { timerGUI.Stop(); timerGUI = null; }
-            timerGUI = new System.Timers.Timer(ms);
-            timerGUI.Elapsed += UpdateGUI;
-            timerGUI.Enabled = true;
-            timerGUI.AutoReset = true;
-            timerGUI.Start();
+                for (int i = 0; i < LAVT.samplesize; i++) {
+                    Line line = new Line();
+                    line.StrokeThickness = 1;
+                    line.Stroke = System.Windows.Media.Brushes.Black;
+                    line.X1 = i;
+                    line.X2 = i;
+                    line.Y1 = 0;
+                    line.Y2 = 15;
+                    canvas1.Children.Add(line);
+                }
+            }*/
         }
 
         /// <summary>
         /// Initialise window
         /// </summary>
-        public MainWindow(){
-            // initialise WPF (XAML)
+        public MainWindow() {
+            // init
+            loaded = false;
             InitializeComponent();
+            loaded = true;
 
-            // Select file
-            filename = selectfile();
+            // setup GUI
+            setInteract(false);
 
-            // If file exists
-            if (filename != "") {
-                // set GUI to loading
-                setLoading();
-
-                // Intialise toolkit
-                LAVT.Initialise(filename);
-            } else {
-                MessageBox.Show("No file selected.", "No file", MessageBoxButton.OK, MessageBoxImage.Warning);
-                LAVT.dump();
-                Application.Current.Shutdown();
-            }
+            F1.IsEnabled = true;
+            
         }
-
-        /// <summary>
-        /// Select a file
-        /// </summary>
-        /// <returns></returns>
-        string? selectfile() {
-            Console.WriteLine("Selecting file...");
-
-            // Configure open file dialog box
-            var dialog = new Microsoft.Win32.OpenFileDialog();
-            dialog.Filter = "Audio files|*.mp3"; // Filter files by extension
-
-            // Show open file dialog box
-            dialog.ShowDialog();
-
-            return dialog.FileName;
-        }
-
+        
         void stop() {
             // stop
             LAVT.StopPlayback();
@@ -154,16 +88,54 @@ namespace Audio_visual_app {
 
         void start() {
             // update
-            LAVT.bands[0].Gain = (int)band1.Value;
+            /*LAVT.bands[0].Gain = (int)band1.Value;
             LAVT.bands[1].Gain = (int)band2.Value;
             LAVT.bands[2].Gain = (int)band3.Value;
             LAVT.bands[3].Gain = (int)band4.Value;
             LAVT.bands[4].Gain = (int)band5.Value;
-            LAVT.equalizer.Update();
+            LAVT.equalizer.Update();*/
+
+            // GUI
+            B1.Content = "Stop";
 
             // start
             LAVT.StartPlayback();
-            B1.Content = "Stop";
+        }
+
+        private void SelectFile(object sender, RoutedEventArgs e) {
+            if (active) { Toggle(null, null); }
+
+            // Configure
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.Filter = "Audio files|*.mp3"; // Filter files by extension
+
+            // Show
+            dialog.ShowDialog();
+
+            // file
+            string filename = dialog.FileName;
+
+            // If file exists
+            if (filename.Length > 0) {
+                // THREAD GOES HERE
+
+                // Intialise toolkit
+                LAVT.dump();
+                LAVT.Initialise(filename);
+
+                // GUI
+                var button = sender as Button;
+                var s = System.IO.Path.GetFileName(filename);
+                if (s.Length > 10) { s = s[..10] + "..."; }
+                button.Content = s;
+                Pslider.Maximum = LAVT.duration;
+
+
+                // settings
+                qualityvalue(null, null);
+            } else {
+                MessageBox.Show("No file selected.", "No file", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
         }
 
         /// <summary>
@@ -181,27 +153,6 @@ namespace Audio_visual_app {
             active = !active;
         }
 
-        void setLoaded() {
-            B1.IsEnabled = true;
-
-            check1.IsChecked = true;
-            check1.Content = "Loaded.";
-            check2.IsChecked = true;
-            check2.Content = "Loaded.";
-            check3.IsChecked = true;
-            check3.Content = "Loaded.";
-            band1.IsEnabled = true;
-            band2.IsEnabled = true;
-            band3.IsEnabled = true;
-            band4.IsEnabled = true;
-            band5.IsEnabled = true;
-
-            check3.Content = "BPM: " + LAVT.BPM;
-            slider1.Maximum = LAVT.duration;
-
-            qslider.IsEnabled = true;
-        }
-
         /// <summary>
         /// Update GUI
         /// </summary>
@@ -209,51 +160,40 @@ namespace Audio_visual_app {
         /// <param name="e"></param>
         private void UpdateGUI(object? sender, EventArgs e) {
             Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() => {
+                // Loaded GUI
+                if (LAVT.analysed) {
+                    if (seconds < LAVT.duration && active) {
+                        // Select data
+                        LAVT.data_struct d = LAVT.data[seconds][(milliseconds / 1000) * quality]; // ERROR
 
-            qslider.Value = quality;
+                        // use data
+                        //check1.IsChecked = d.onset;
+                        //check1.Content = "Onset: " + check1.IsChecked;
+                        //check2.Content = "Frequency: " + d.frequency;
 
-            // Load GUI
-            if (LAVT.analysed && !loaded) {
-                // Set GUI to loaded
-                setLoaded();
-                loaded = true;
-            }
+                        // get visuals
+                        /*Line[] l = canvas1.Children.OfType<Line>().ToArray();
 
-            // Loaded GUI
-            if (loaded) {
-                if (seconds < LAVT.duration && active) {
-                    // Select data
-                    LAVT.data_struct d = LAVT.data[seconds][milliseconds / wait]; // ERROR
+                        // update each line according to pcm data
+                        for (int j = 0; j < LAVT.samplesize; j++) {
+                            l[j].Y2 = d.sample[j];
+                        }*/
 
-                    // use data
-                    check1.IsChecked = d.onset;
-                    check1.Content = "Onset: " + check1.IsChecked;
-                    check2.Content = "Frequency: " + d.frequency;
+                        // Timing
+                        milliseconds += wait;
 
-                    // get visuals
-                    Line[] l = canvas1.Children.OfType<Line>().ToArray();
+                        if (milliseconds >= 1000 - wait) {
+                            seconds += 1;
+                            milliseconds = 0;
+                        }
 
-                    // update each line according to pcm data
-                    for (int j = 0; j < LAVT.samplesize; j++) {
-                        l[j].Y2 = d.sample[j];
+                        // GUI
+                        Pslider.Value = seconds;
+                        T1.Text = seconds + " (" + milliseconds + "ms / 1 second" + ") / " + LAVT.duration;
+                    } else if (active) {
+                        Toggle(null, null);
                     }
-
-                    // Timing
-                    milliseconds += wait;
-
-                    if (milliseconds >= 1000 - wait) {
-                        seconds += 1;
-                        milliseconds = 0;
-                    }
-
-                    // GUI
-                    slider1.Value = seconds;
-                    label1.Content = seconds + " (" + milliseconds + "ms / 1 second" + ") / " + LAVT.duration;
-                } else if (active) {
-                    Toggle(null, null);
                 }
-            }
-
             }));
         }
 
@@ -278,20 +218,32 @@ namespace Audio_visual_app {
             if (active) { Toggle(null, null); }
         }
 
-        private void qualityvalue(object sender, RoutedPropertyChangedEventArgs<double> e) {
-            // load
-            if (active) { Toggle(null, null); }
-            setLoading();
+        private void qualityvalue(object sender, RoutedPropertyChangedEventArgs<double> e) { // ERROR
+            if (loaded) {
+                // load
+                if (active) { Toggle(null, null); }
 
-            // set quality
-            var sl = sender as Slider;
-            quality = (int)sl.Value;
-            
-            Thread thread = new Thread(() => {
-                setQuality(quality);
-            });
+                // GUI
+                quality = (int)qslider.Value;
+                Q1.Text = "Quality: " + quality;
 
-            thread.Start();
+                // THREAD GOES HERE
+
+                // quality
+                quality = quality;
+                LAVT.setData(quality);
+
+                // timer
+                wait = 1000 / quality;
+                if (timerGUI != null) { timerGUI.Stop(); timerGUI = null; }
+                timerGUI = new System.Timers.Timer(wait);
+                timerGUI.Elapsed += UpdateGUI;
+                timerGUI.Enabled = true;
+                timerGUI.AutoReset = true;
+                timerGUI.Start();
+
+                setInteract(true);
+            }
         }
     }
 }
